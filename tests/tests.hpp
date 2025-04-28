c8182415b9e39286d6ab3a7b3845b5091f3aa541
yayamayaya <semenov.aleksandr@phystech.edu>
2025-04-29 01:13:41 +0300

    Сделал первый тест грамматики с контекстами

--- tests/tests.hpp
+++ tests/tests.hpp
@@ -10,9 +10,6 @@ TEST(context_free_tests, first_test)
     // A{0x01} -> BC
     // B{0x02} -> "b"
     // C{0x03} -> "c"
-    //
-    //
-    //
 
     grammar g = {};
     g.add_terminal_grammar(0x02, {std::make_pair<std::string, std::vector<context_t>>("b", {})});
@@ -23,6 +20,31 @@ TEST(context_free_tests, first_test)
 
     p.set_grammar(g);
     p.parse_string("b c");
+}
+
+TEST(context_sensitive_grammar, simple_test)
+{
+    // S -> AS | SA | BC
+    // A -> "a"
+    // B -> "b" < A
+    // C -> "c" > A
+
+    grammar g = {};
+    g.add_non_terminal_grammar(0x01, 
+        {
+            std::make_pair<grammar_pair_t, std::vector<context_t>>({0x02, 0x01}, {}),
+            std::make_pair<grammar_pair_t, std::vector<context_t>>({0x01, 0x02}, {}),
+            std::make_pair<grammar_pair_t, std::vector<context_t>>({0x03, 0x04}, {}),
+        });
+
+    g.add_terminal_grammar(0x02, {std::make_pair<std::string, std::vector<context_t>>("a", {})});
+    g.add_terminal_grammar(0x03, {std::make_pair<std::string, std::vector<context_t>>("b", {{LEFT, 0x02}})});
+    g.add_terminal_grammar(0x04, {std::make_pair<std::string, std::vector<context_t>>("c", {{RIGHT, 0x02}})});
+
+    parser p = {};
+
+    p.set_grammar(g);
+    p.parse_string("a b c a");
 }
 
 #endif
