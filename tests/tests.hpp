60fd9f0ff6f9d87404c47c53a5fa2157e725610c
yayamayaya <semenov.aleksandr@phystech.edu>
2025-04-28 23:49:28 +0300

    добавил файлы для тестов, начал работу над первым тестым

---
+++ tests/tests.hpp
@@ -0,0 +1,28 @@
+#ifndef GOOGLE_TESTING_HEADER
+#define GOOGLE_TESTING_HEADER
+
+#include <gtest/gtest.h>
+#include "parser.hpp"
+#include "grammar.hpp"
+
+TEST(context_free_tests, first_test)
+{
+    // A{0x01} -> BC
+    // B{0x02} -> "b"
+    // C{0x03} -> "c"
+    //
+    //
+    //
+
+    grammar g = {};
+    g.add_terminal_grammar(0x02, {std::make_pair<std::string, std::vector<context_t>>("b", {})});
+    g.add_terminal_grammar(0x03, {std::make_pair<std::string, std::vector<context_t>>("c", {})});
+    g.add_non_terminal_grammar(0x01, {std::make_pair<grammar_pair_t, std::vector<context_t>>({0x02, 0x03}, {})});
+
+    parser p = {};
+
+    p.set_grammar(g);
+    p.parse_string("b c");
+}
+
+#endif
