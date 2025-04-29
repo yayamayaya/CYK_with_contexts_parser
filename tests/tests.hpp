2ab8e9d3ba268cd7d5e427a32f058906ce0635a6
yayamayaya <semenov.aleksandr@phystech.edu>
2025-04-29 15:19:26 +0300

    добавил тестов

--- tests/tests.hpp
+++ tests/tests.hpp
@@ -5,7 +5,7 @@
 #include "parser.hpp"
 #include "grammar.hpp"
 
-TEST(context_free_tests, first_test)
+TEST(context_free_tests, simple_test1)
 {
     // A{0x01} -> BC
     // B{0x02} -> "b"
@@ -19,7 +19,24 @@ TEST(context_free_tests, first_test)
     parser p = {};
 
     p.set_grammar(g);
-    p.parse_string("b c");
+    EXPECT_EQ(p.parse_string("b c"), 0);
+}
+
+TEST(context_free_tests, negative_test1)
+{
+    // A{0x01} -> BC
+    // B{0x02} -> "b"
+    // C{0x03} -> "c"
+
+    grammar g = {};
+    g.add_terminal_grammar(0x02, {std::make_pair<std::string, std::vector<context_t>>("b", {})});
+    g.add_terminal_grammar(0x03, {std::make_pair<std::string, std::vector<context_t>>("c", {})});
+    g.add_non_terminal_grammar(0x01, {std::make_pair<grammar_pair_t, std::vector<context_t>>({0x02, 0x03}, {})});
+
+    parser p = {};
+
+    p.set_grammar(g);
+    EXPECT_EQ(p.parse_string("b d"), parser::NOT_PARSED);
 }
 
 TEST(context_sensitive_grammar, simple_test1)
@@ -44,7 +61,7 @@ TEST(context_sensitive_grammar, simple_test1)
     parser p = {};
 
     p.set_grammar(g);
-    p.parse_string("a b c a");
+    EXPECT_EQ(p.parse_string("a b c a"), 0);
 }
 
 TEST(context_sensitive_grammar, simple_test2)
@@ -64,7 +81,32 @@ TEST(context_sensitive_grammar, simple_test2)
     parser p = {};
 
     p.set_grammar(g);
-    p.parse_string("a b");
+    EXPECT_EQ(p.parse_string("a b"), 0);
+}
+
+TEST(context_sensitive_grammar, negative_test2)
+{
+    // S -> AS | SA | BC
+    // A -> "a"
+    // B -> "b" < A
+    // C -> "c" > A
+
+    grammar g = {};
+    g.add_non_terminal_grammar(0x01, 
+        {
+            std::make_pair<grammar_pair_t, std::vector<context_t>>({0x02, 0x01}, {}),
+            std::make_pair<grammar_pair_t, std::vector<context_t>>({0x01, 0x02}, {}),
+            std::make_pair<grammar_pair_t, std::vector<context_t>>({0x03, 0x04}, {}),
+        });
+
+    g.add_terminal_grammar(0x02, {std::make_pair<std::string, std::vector<context_t>>("a", {})});
+    g.add_terminal_grammar(0x03, {std::make_pair<std::string, std::vector<context_t>>("b", {{LEFT, 0x02}})});
+    g.add_terminal_grammar(0x04, {std::make_pair<std::string, std::vector<context_t>>("c", {{RIGHT, 0x02}})});
+
+    parser p = {};
+
+    p.set_grammar(g);
+    EXPECT_EQ(p.parse_string("b c a"), parser::NOT_PARSED);
 }
 
 #endif
