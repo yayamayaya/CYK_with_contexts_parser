2e5a488a0b3ff3d965d83c88f69653e1a79a8b72
yayamayaya <semenov.aleksandr@phystech.edu>
2025-05-01 18:55:28 +0300

    добавил ещё тестов

--- tests/tests.hpp
+++ tests/tests.hpp
@@ -84,6 +84,23 @@ TEST(context_sensitive_grammar, simple_test2)
     EXPECT_EQ(p.parse_string("a b"), 0);
 }
 
+TEST(context_sensitive_grammar, simple_test3)
+{
+    //S -> AS | "a"
+    //A -> "a" > S
+
+    grammar g = {};
+    g.add_non_terminal_grammar(0x01, {std::make_pair<grammar_pair_t, std::vector<context_t>>({0x02, 0x01}, {})});
+
+    g.add_terminal_grammar(0x01, {std::make_pair<std::string, std::vector<context_t>>("a", {})});
+    g.add_terminal_grammar(0x02, {std::make_pair<std::string, std::vector<context_t>>("a", {{RIGHT, 0x01}})});
+
+    parser p = {};
+
+    p.set_grammar(g);
+    EXPECT_EQ(p.parse_string("a a a a"), 0);
+}
+
 TEST(context_sensitive_grammar, negative_test2)
 {
     // S -> AS | SA | BC
@@ -107,6 +124,32 @@ TEST(context_sensitive_grammar, negative_test2)
 
     p.set_grammar(g);
     EXPECT_EQ(p.parse_string("b c a"), parser::NOT_PARSED);
+}
+
+TEST(graph_print_test, only_test)
+{
+    // S -> AS | SA | BC
+    // A -> "a"
+    // B -> "b" < A
+    // C -> "c" > A
+
+    grammar g = {};
+    g.add_non_terminal_grammar(0x01, 
+        {
+            std::make_pair<grammar_pair_t, std::vector<context_t>>({0x02, 0x01}, {}),
+            std::make_pair<grammar_pair_t, std::vector<context_t>>({0x01, 0x02}, {}),
+            std::make_pair<grammar_pair_t, std::vector<context_t>>({0x03, 0x04}, {}),
+        });
+
+    g.add_terminal_grammar(0x02, {std::make_pair<std::string, std::vector<context_t>>("a", {})});
+    g.add_terminal_grammar(0x03, {std::make_pair<std::string, std::vector<context_t>>("b", {{LEFT, 0x02}})});
+    g.add_terminal_grammar(0x04, {std::make_pair<std::string, std::vector<context_t>>("c", {{RIGHT, 0x02}})});
+
+    parser p = {};
+
+    p.set_grammar(g);
+    EXPECT_EQ(p.parse_string("a b c a"), 0);
+    EXPECT_NO_THROW(p.print_graph("./graph.png"));
 }
 
 #endif
